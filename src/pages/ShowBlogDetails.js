0a71aba821e6be4f62a32bb465cf435fdcd14efd
Shashank-g1 <shashank.gupta@sendinblue.com>
2021-12-29 12:56:41 +0530

    some minor changes

--- src/pages/ShowBlogDetails.js
+++ src/pages/ShowBlogDetails.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useParams } from "react-router";
+import styles from "./ShowBlogDetails.module.less";
 
 function ShowBlogDetails() {
   const id = useParams();
 
-  console.log(id.id);
-
   return (
-    <div>
-      <p>Hello Guest</p>
+    <div className={styles.container}>
+      <h3>Hello user {id.id}</h3>
+      <p> </p>
     </div>
   );
 }
